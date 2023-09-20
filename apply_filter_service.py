1acd1c106e2aef231aa2467019f68d7ef19e6f73
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2023-09-19 22:36:25 -0300

    all inputed image to grayscale

--- apply_filter_service.py
+++ apply_filter_service.py
@@ -3,11 +3,12 @@ from filters import negative
 
 ALTERED_FOLDER = "./altered"
 
+
 def execute(filter, image_name):
     if not filter or not image_name:
         return False
-    
-    if filter == 'negative':
+
+    if filter == "negative":
         new_image = negative(image_name)
         new_image.save(os.path.join(ALTERED_FOLDER, image_name))
 
--- filters.py
+++ filters.py
@@ -4,16 +4,17 @@ UPLOAD_FOLDER = "./uploads"
 
 
 def getImage(image_name):
-    return Image.open(UPLOAD_FOLDER + "/" + image_name)
+    return Image.open(UPLOAD_FOLDER + "/" + image_name).convert("L")
+
 
 def negative(image_name):
     image = getImage(image_name)
     copy = image.copy()
 
-    for i in range(0, image.size[0]-1):
-        for j in range(0, image.size[1]-1):        
-            original_pixel = image.getpixel((i,j))
+    for i in range(0, image.size[0] - 1):
+        for j in range(0, image.size[1] - 1):
+            original_pixel = image.getpixel((i, j))
             output_pixel = 255 - original_pixel
-            copy.putpixel((i,j), output_pixel)
-    
+            copy.putpixel((i, j), output_pixel)
+
     return copy
