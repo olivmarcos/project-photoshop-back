520d43081fd8c05aee939623efe6c0cbb13ae1f0
Marcos Oliveira <marcos.oliveira@invillia.com>
2023-09-21 14:24:47 -0300

    add filters validation to back end

--- apply_filter_service.py
+++ apply_filter_service.py
@@ -2,10 +2,16 @@ import os
 from filters import negative, logarithm
 
 ALTERED_FOLDER = "./altered"
+ALLOWED_FILTERS = ['negative', 'logarithm', 'inverse-logarithm', 'power', 'root']
 
+def allowed_filter(filter):
+    return filter in ALLOWED_FILTERS
 
 def execute(filter, image_name):
     if not filter or not image_name:
+        return False
+    
+    if not allowed_filter(filter):
         return False
 
     if filter == "negative":
@@ -17,4 +23,16 @@ def execute(filter, image_name):
         new_image = logarithm(image_name)
         new_image.save(os.path.join(ALTERED_FOLDER, image_name))
 
+    if filter == "inverse-logarithm":
+        new_image = logarithm(image_name)
+        # TO-DO
+
+    if filter == "power":
+        new_image = logarithm(image_name)
+        # TO-DO
+
+    if filter == "root":
+        new_image = logarithm(image_name)
+        # TO-DO
+        
     return image_name
