7c71bd8680011eebe9113bacf581dfadf4930ef7
Marcos Oliveira <marcos.oliveira@invillia.com>
2023-09-21 21:58:41 -0300

    add more filters

--- apply_filter_service.py
+++ apply_filter_service.py
@@ -2,7 +2,17 @@ import os
 import filters
 
 ALTERED_FOLDER = "./altered"
-ALLOWED_FILTERS = ["negative", "logarithm", "inverse-logarithm", "power", "root"]
+
+ALLOWED_FILTERS = [
+    "negative",
+    "logarithm",
+    "inverse-logarithm",
+    "power",
+    "root",
+    "rotation-ninety-degree",
+    "rotation-counterclockwise-ninety-degree",
+    "rotation-one-hundred-eighty",
+]
 
 
 def allowed_filter(filter):
@@ -36,6 +46,18 @@ def execute(filter, image_name, gamma):
         new_image.save(os.path.join(ALTERED_FOLDER, image_name))
 
     if filter == "root":
+        new_image = filters.root(image_name, gamma)
+        new_image.save(os.path.join(ALTERED_FOLDER, image_name))
+
+    if filter == "rotation-ninety-degree":
+        new_image = filters.rotation_ninety_degree(image_name, gamma)
+        new_image.save(os.path.join(ALTERED_FOLDER, image_name))
+
+    if filter == "rotation-counterclockwise-ninety-degree":
+        new_image = filters.rotation_counterclockwise_ninety_degree(image_name, gamma)
+        new_image.save(os.path.join(ALTERED_FOLDER, image_name))
+
+    if filter == "rotation-one-hundred-eighty":
         new_image = filters.root(image_name, gamma)
         new_image.save(os.path.join(ALTERED_FOLDER, image_name))
 
